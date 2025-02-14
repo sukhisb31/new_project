f5efa7f4f9344557b1259f0c16be8b668150282d
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-02-14 15:02:20 +0530

    till navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import Signup from "./Components/pages/Form/Signup/Signup";
+import { Navbar } from "./Components/pages/Navbar/Navbar"
+import { BrowserRouter,Route, Routes } from "react-router-dom";
 
 
 const App = () => {
   return (
-    <div>
+    <>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
       
-    </div>
+    </>
   )
 }
 
